598db04a63d304521a697730cc2a404e16061bb3
kapanadze <kapan.gio777@gmail.com>
2021-08-03 15:16:26 +0400

    Add files via upload

---
+++ data/hpp_5.js
@@ -0,0 +1,17 @@
+var json_hpp_5 = 
+{
+    "type": "FeatureCollection",
+    "name": "test1",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": [
+    { "type": "Feature", "properties": { "id": "", "Project Name:": "Sevan Lake", "Project Type:": "HPP", "Project Area:": "Armenia", "Full info:": "https:\/\/greenalt.org\/" }, "geometry": { "type": "Point", "coordinates": [ 45.334161119722829, 40.368470028192604 ] } },
+    { "type": "Feature", "properties": { "id": "", "Project Name:": "Namakhvani Hydropower plant ", "Project Type:": "HPP", "Project Area:": "Georgia", "Full info:": "https:\/\/greenalt.org\/" }, "geometry": { "type": "Point", "coordinates": [ 42.713088523639911, 42.424211577153251 ] } },
+    { "type": "Feature", "properties": { "id": "", "Project Name:": "Vorotan Hydrocascade", "Project Type:": "HPP", "Project Area:": "Armenia", "Full info:": "https:\/\/greenalt.org\/" }, "geometry": { "type": "Point", "coordinates": [ 45.913066047902575, 39.573624226544894 ] } },
+    { "type": "Feature", "properties": { "id": "", "Project Name:": "Shuakhevi Hydropower plant ", "Project Type:": "HPP", "Project Area:": "Georgia", "Full info:": "https:\/\/greenalt.org\/" }, "geometry": { "type": "Point", "coordinates": [ 42.349599812327391, 41.657941683941758 ] } },
+    { "type": "Feature", "properties": { "id": "", "Project Name:": "Dariali Hydropower plant", "Project Type:": "HPP", "Project Area:": "Georgia", "Full info:": "https:\/\/greenalt.org\/" }, "geometry": { "type": "Point", "coordinates": [ 44.623249076739171, 42.744689120530168 ] } },
+    { "type": "Feature", "properties": { "id": "", "Project Name:": "Nenskra Hydropower plant", "Project Type:": "HPP", "Project Area:": "Georgia", "Full info:": "https:\/\/greenalt.org\/" }, "geometry": { "type": "Point", "coordinates": [ 42.186352614066102, 43.056998289965868 ] } },
+    { "type": "Feature", "properties": { "id": "", "Project Name:": "Oni Hydropower plant cascade", "Project Type:": "HPP", "Project Area:": "Georgia", "Full info:": "https:\/\/greenalt.org\/" }, "geometry": { "type": "Point", "coordinates": [ 43.428247680044649, 42.578069666702312 ] } },
+    { "type": "Feature", "properties": { "id": "", "Project Name:": "Shnogh Hydropower Plant", "Project Type:": "HPP", "Project Area:": "Armenia", "Full info:": "https:\/\/greenalt.org\/" }, "geometry": { "type": "Point", "coordinates": [ 44.842997342410939, 41.167760957716894 ] } }
+    ]
+    }
+    
